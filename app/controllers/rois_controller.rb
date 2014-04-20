6f6cb73de4118213c2c1d3838724cf5e4fe761d1
josthedudeoflife <josh.chou@ses-technologies.com>
2014-04-20 18:27:00 -0400

    still having trouble yet again

--- app/controllers/rois_controller.rb
+++ app/controllers/rois_controller.rb
@@ -1,18 +1,30 @@
 class RoisController < ApplicationController
 	def index
-		@rois = ROI.all
+		@rois = Roi.all
   end
   def show
-  	@roi = ROI.find params[:id]
+  	@roi = Roi.find params[:id]
   end
   def new
-  	@roi = ROI.new
+  	@roi = Roi.new
   end
   def show
-  	@roi = ROI.new
+  	@roi = Roi.new
   end
   def create
-  	@roi = ROI.new
+  	@roi = Roi.new
   	@roi.spent = params[:roi][:spent]
+  	@roi.retailers = params[:roi][:retailers]
+  	@roi.purchase_volume = params[:roi][:purchase_volume]
+  	@roi.contribution_margin = params[:roi][:contribution_margin]
+  	@roi.sales_increase = params[:roi][:sales_increase]
+  	@roi.leads = params[:roi][:leads]
+  	@roi.leads_value = params[:roi][:leads_value]
+  	@roi = @roi.spent 
+  	if @roi.save
+  			puts "Your ROI is #{roi}"
+  		else
+  			render :new
+  		end
   end
 end
